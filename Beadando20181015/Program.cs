7642d53c51954084dc5f2e217f521849291b6d01
Valentin Csordás <csordas.valentin@gmail.com>
2018-10-15 02:53:58 +0200

    First Commit

--- Beadando20181015/Program.cs
+++ Beadando20181015/Program.cs
@@ -10,6 +10,23 @@ namespace Beadando20181015
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Kérem adjon meg 10 egész számot: ");
+
+            int max = 0;
+
+            int n = 10;
+            int[] t = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                t[i] = Int32.Parse(Console.ReadLine());
+                max = Math.Max(t[i], max);
+            }
+
+            Console.WriteLine(max);
+
+            Console.ReadLine();
+
         }
     }
 }
